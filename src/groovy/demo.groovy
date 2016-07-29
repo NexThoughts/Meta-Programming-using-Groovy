965c0080c24d1aa35f55ab65661bf2d744472778
Ali Tanwir <ali.tanwir@nexthoughts.com>
2016-07-29 16:37:39 +0530

    Added Method Delegation example 2

--- src/groovy/demo.groovy
+++ src/groovy/demo.groovy
@@ -268,8 +268,7 @@ assert !p.hasProperty('country')*/
 //=============================================
 // Closure Delegate Demo
 //=============================================
-/*
-cl = { ->
+/*cl = { ->
     append "Hi!"
     append " this is closure delegate demo."
 }
@@ -282,6 +281,66 @@ cl()
 println "SB: ${sb}"*/
 
 //=============================================
+// Method Delegation
+//=============================================
+class Worker {
+    def simpleWork1(spec) { println "worker does work1 with spec $spec" }
+
+    def simpleWork2() { println "worker does work2" }
+}
+
+class Expert {
+    def advancedWork1(spec) { println "Expert does work1 with spec $spec" }
+
+    def advancedWork2(scope, spec) {
+        println "Expert does work2 with scope $scope spec $spec"
+    }
+}
+
+class Manager {
+    def worker = new Worker()
+    def expert = new Expert()
+
+    def schedule() { println "Scheduling ..." }
+
+    def methodMissing(String name, args) {
+        println "intercepting call to $name..."
+        def delegateTo = null
+        if (name.startsWith('simple')) {
+            delegateTo = worker
+        }
+        if (name.startsWith('advanced')) {
+            delegateTo = expert
+        }
+        if (delegateTo?.metaClass.respondsTo(delegateTo, name, args)) {
+            Manager instance = this
+            instance.metaClass."${name}" = { Object[] varArgs ->
+                delegateTo.invokeMethod(name, varArgs)
+            }
+            delegateTo.invokeMethod(name, args)
+        } else {
+            throw new MissingMethodException(name, Manager.class, args)
+        }
+    }
+}
+
+peter = new Manager()
+peter.schedule()
+peter.simpleWork1('fast')
+peter.simpleWork1('quality')
+peter.simpleWork2()
+peter.simpleWork2()
+peter.advancedWork1('fast')
+peter.advancedWork1('quality')
+peter.advancedWork2('prototype', 'fast')
+peter.advancedWork2('product', 'quality')
+try {
+    peter.simpleWork3()
+} catch (Exception ex) {
+    println ex
+}
+
+//=============================================
 // Creating Dynamic Classes with Expando
 //=============================================
 /*
@@ -292,10 +351,11 @@ carA.miles = 10
 println "carA: " + carA
 println "carB: " + carB*/
 
-car = new Expando(year: 2012, miles: 0, turn: { println 'turning...' })
+/*car = new Expando(year: 2012, miles: 0, turn: { println 'turning...' })
 car.drive = {
     miles += 10
     println "$miles miles driven"
 }
 car.drive()
-car.turn()
+car.turn()*/
+
