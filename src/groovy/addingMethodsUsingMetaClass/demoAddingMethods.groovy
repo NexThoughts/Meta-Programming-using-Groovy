9ec9c061ae1fc669ea328c38bab82774856da107
Ali Tanwir <ali.tanwir@nexthoughts.com>
2016-07-29 17:43:39 +0530

    Moved to Adding Methods package

---
+++ src/groovy/addingMethodsUsingMetaClass/demoAddingMethods.groovy
@@ -0,0 +1,25 @@
+package addingMethodsUsingMetaClass
+
+/**
+ * Created by ali on 29/7/16.
+ */
+//=============================================
+// Adding Methods Using MetaClass
+//=============================================
+class StringUtils {
+    static String truncate(String text, Integer length, Boolean overflow = false) {
+        text.take(length) + (overflow ? '...' : '')
+    }
+}
+
+String chuckIpsum = "If you can see Chuck Norris, he can see you.\
+If you can not see Chuck Norris you may be only seconds away from death"
+println StringUtils.truncate(chuckIpsum, 72)
+println StringUtils.truncate(chuckIpsum, 72, true)
+
+String.metaClass.truncateDemo = { Integer length, Boolean overflow = false ->
+    println "truncate string upto length $length"
+    delegate.take(length) + (overflow ? '...' : '')
+}
+assert chuckIpsum.truncateDemo(20, true) == StringUtils.truncate(chuckIpsum, 20, true)
+println(chuckIpsum.truncateDemo(20, true))
