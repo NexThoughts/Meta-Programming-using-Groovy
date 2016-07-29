da9691ebe2fed15f0d9b24dba3bed67bcbafe36e
Ali Tanwir <ali.tanwir@nexthoughts.com>
2016-07-30 02:29:21 +0530

    added example for Adding Static Method Using ExpandoMetaClass

--- src/groovy/addingConstructorUsingMetaClass/demoAddingConstructor.groovy
+++ src/groovy/addingConstructorUsingMetaClass/demoAddingConstructor.groovy
@@ -16,3 +16,11 @@ Integer.metaClass.constructor << { Calendar calendar ->
     new Integer(calendar.get(Calendar.DATE))
 }
 println("Today's Date: ${new Integer(Calendar.instance)}")
+
+//=============================================
+// Adding Static Method Using ExpandoMetaClass
+//=============================================
+/*
+Integer.metaClass.'static'.isEven = { val -> val % 2 == 0 }
+println "Is 2 even? " + Integer.isEven(2)
+println "Is 3 even? " + Integer.isEven(3)*/
