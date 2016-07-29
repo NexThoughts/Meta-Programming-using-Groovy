f2744c58f22f214af55dd0c5de3650180bb9c851
Ali Tanwir <ali.tanwir@nexthoughts.com>
2016-07-30 03:15:06 +0530

    injecting method getDaysFromNow using ExpandoMetaClass

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,11 @@
 class BootStrap {
 
     def init = { servletContext ->
+        Integer.metaClass.getDaysFromNow = { ->
+            Calendar today = Calendar.instance
+            today.add(Calendar.DAY_OF_MONTH, delegate)
+            today.time
+        }
     }
     def destroy = {
     }
